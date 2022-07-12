b724affe8b31550f07e1b748f1269f8c0b4dac46
Chris Clynes <chrisclynes@live.com>
2022-07-11 21:10:53 -0400

    working on header styles

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,18 +9,18 @@ const Header = () => {
             <div className="app__header-badge">
                    <div className="app__title app__flex">
                         <div style={{ marginLeft: 20 }}>
-                            <p className="head-text2">Hello, I am</p>
+                            <p className="head-text2">Hello, I'm</p>
                             <h1 className="head-text">Chris</h1>
                         </div>
                     </div> 
                 <motion.div
                     initial={{ x: -500, opacity: 0}}
-                    animate={{x: 0, opacity: 1}}
+                    animate={{ x: 0, opacity: 1 }}
                     transition={{ type: 'spring', delay: 1, duration: 1, bounce: 0.5 }}
                     className="app__header-info"
                 >
                     <div className="sub-tag app__flex">
-                        <p className="p-text">A Web Developer</p>
+                        <p className="logo-text" >A Web Developer</p>
                     </div>
                 </motion.div>
             </div>
